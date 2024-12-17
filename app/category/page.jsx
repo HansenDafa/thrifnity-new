41ebf884ff50681b98d9e6747191a90834ab7eed
rizaldonii <rizaldonii24@gmail.com>
2024-12-18 01:19:56 +0700

    change name /shop to /category, fixing link

--- app/category/page.jsx
+++ app/category/page.jsx
@@ -1,9 +1,9 @@
 import { categories, locations, products } from '@/libs/constant';
 
-import Button from '@/components/ui/button';
-import Link from 'next/link';
 import { ProductCard } from '@/components/shop/product-card';
 import { Sidebar } from '@/components/shop/sidebar';
+import Button from '@/components/ui/button';
+import Link from 'next/link';
 
 export default function ShopPage() {
 	return (
@@ -22,7 +22,7 @@ export default function ShopPage() {
 				<div className='lg:col-span-3 xl:col-span-4'>
 					<div className='grid gap-6 mb-6 sm:grid-cols-2 xl:grid-cols-4'>
 						{products.map((product) => (
-							<Link href={'/delivery/' + product.id}>
+							<Link href={'/productdetails/'}>
 								<ProductCard
 									key={product.id}
 									product={product}
